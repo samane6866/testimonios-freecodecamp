6bf0dcb0e42c80929ad225f71231ecc6e38561aa
samane6866 <samane.bahramian@gmail.com>
2023-05-17 19:38:57 +0200

    first component done

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -7,6 +7,14 @@ function Testimonio() {
         className="imagen-testimonio"
         src={require("../imagenes/Untitled1.jpeg")}
       />
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
+        <p className="cargo-testimonio">Ingenero de software</p>
+        <p className="texto-testimonio">
+          siempre he tenido problemas en aprender javascript
+        </p>
+      </div>
     </div>
   );
 }
+export default Testimonio;
