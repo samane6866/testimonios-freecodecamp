aae1e4f63fd0c5e5152947a76eb08487785590c4
samane6866 <samane.bahramian@gmail.com>
2023-05-17 19:14:20 +0200

    creat components

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,12 @@
+import reaact from "react";
+
+function Testimonio() {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require("../imagenes/Untitled1.jpeg")}
+      />
+    </div>
+  );
+}
