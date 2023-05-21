f3148d064c58d0881cf80917d4d5f1ac38c376e3
samane6866 <samane.bahramian@gmail.com>
2023-05-21 21:58:05 +0200

    props testimonio done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,30 @@ function App() {
     <div className="App">
       <div className="contenedor-principal">
         <h1 className="">Esto es lo que dicen nuestro alumnos: </h1>
-        <Testimonio />
+        <Testimonio
+          nombre="Emaa Bostia"
+          pais="Suecia"
+          imagen="Untitled1"
+          cargo=" Softwer developer"
+          empresa="Spotify"
+          testimonio="Siempre he tenido problemas para aprender JavaScript con cursos de freeCodeCamp he podido aprender mucho."
+        />
+        <Testimonio
+          nombre="Show Wang"
+          pais="Singapur"
+          imagen="Untitled2"
+          cargo="Ingeniero de Software"
+          empresa="Amazon"
+          testimonio="Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida."
+        />
+        <Testimonio
+          nombre="Sarah"
+          pais="Nigeria"
+          imagen="Sarah"
+          cargo=" Ingienera de software"
+          empresa="Spotify"
+          testimonio="freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble."
+        />
       </div>
     </div>
   );
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,18 +1,20 @@
 import react from "react";
 import "/home/samane/PIMEC2023/proyectos-free-code-camp/testimonios-freecodecamp/src/hojas-de-estilo/Testimonio.css";
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
       <img
         className="imagen-testimonio"
-        src={require("../imagenes/Untitled1.jpeg")}
+        src={require(`../imagenes/${props.imagen}.jpeg`)}
       />
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-        <p className="cargo-testimonio">Ingenero de software</p>
-        <p className="texto-testimonio">
-          siempre he tenido problemas en aprender javascript
+        <p className="nombre-testimonio">
+          <strong> {props.nombre}</strong> en {props.pais}
         </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio">{props.testimonio}</p>
       </div>
     </div>
   );
